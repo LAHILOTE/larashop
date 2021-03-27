cb5469e75a52202abe5a19606d5bb02326196b34
mudherdomili <mudher.d@gmail.com>
2021-03-27 13:43:32 +0800

    Perbaikan Fitur Delete Fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -116,7 +116,7 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
-        $user = User::findOrFail();
+        $user = User::findOrFail($id);
         $user->delete();
         return redirect()->route('users.index')->with('status', 'User successfully deleted');
     }
