ba2c6c911647ec1673864f82573eb4af215546b1
mudherdomili <mudher.d@gmail.com>
2021-04-02 23:58:08 +0800

    Halaman Create Category

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -3,7 +3,26 @@
 @section('title') Create Category @endsection
 
 @section('content')
+    <div class="col-md-8">
+        @if (session('status'))
+            <div class="alert alert-success">
+                {{ session('status') }}
+            </div>
+        @endif
+        <form enctype="multipart/form-data" action="{{ route('categories.store') }}" class="bg-white shadow-sm p-3"
+            method="POST">
+            @csrf
 
-TODO : Form create category
+            <label for="">Category name</label>
+            <input type="text" name="name" class="form-control">
+            <br>
 
+            <label for="">Category image</label>
+            <input type="file" name="image" class="form-control">
+            <br>
+
+            <input type="submit" class="btn btn-primary" value="Save">
+
+        </form>
+    </div>
 @endsection
