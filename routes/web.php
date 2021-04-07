e8d7b627c4380614d375ae4d153dd2fef8a7172b
mudherdomili <mudher.d@gmail.com>
2021-04-07 18:30:58 +0800

    Penambahan Route Ajak Input type select

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ Route::get('/categories/trash', [CategoryController::class, 'trash'])->name('cat
 Route::get('/categories/{id}/restore', [CategoryController::class, 'restore'])->name('categories.restore');
 Route::delete('/categories/{category}/delete-permanent',[CategoryController::class, 'deletePermanent'])->name('categories.delete-permanent');
 
+Route::get('/ajax/categories/search', [CategoryController::class, 'ajaxSearch']);
 
 Route::resource("users", UserController::class);
 Route::resource("categories", CategoryController::class);
