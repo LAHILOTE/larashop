f741c3263312f590f76a3be9d760529871e446bf
mudherdomili <mudher.d@gmail.com>
2021-04-08 01:13:54 +0800

    Add Fungsi softdelete & relasi ke tabel Order

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,12 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Book extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
-    public function categories(){
+    public function categories()
+    {
         return $this->belongsToMany('App\Models\Category');
+    }
+
+    public function orders()
+    {
+        return $this->belongsToMany('App\Models\Order');
     }
 }
