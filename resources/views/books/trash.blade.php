fca8c2b64fce57ae6de644d54a241c562edacd31
mudherdomili <mudher.d@gmail.com>
2021-04-08 01:12:36 +0800

    Halaman View Trash Books

---
+++ resources/views/books/trash.blade.php
@@ -0,0 +1,121 @@
+@extends('layouts.global')
+
+@section('title')
+    Trashed Books
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+
+            <div class="row">
+                <div class="col-md-6">
+                    <form action="{{ route('books.index') }}">
+                        <div class="input-group">
+                            <input type="text" name="keyword" value="{{ Request::get('keyword') }}" class="form-control"
+                                placeholder="Filter by title">
+                            <input type="submit" value="Filter" class="btn btn-primary">
+                        </div>
+                    </form>
+                </div>
+                <div class="col-md-6">
+                    <ul class="nav nav-pills card-header-pills">
+                        <li class="nav-item">
+                            <a href="{{ route('books.index') }}" class="nav-link">ALL</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('books.index', ['status' => 'publish']) }}" class="nav-link">Publish</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('books.index', ['status' => 'draft']) }}" class="nav-link">Draft</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('books.trash') }}"
+                                class="nav-link {{ Request::path() == 'books/trash' ? 'active' : '' }}">Trash</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <hr class="my-3">
+
+
+
+            <div class="row mb-3">
+                <div class="col-md-12 text-right">
+                    <a href="{{ route('books.create') }}" class="btn btn-primary">Create Book</a>
+                </div>
+            </div>
+
+            @if (session('status'))
+                <div class="alert alert-success">
+                    {{ session('status') }}
+                </div>
+            @endif
+
+            <table class="table table-bordered table-striped">
+                <thead>
+                    <tr>
+                        <th><b>Cover</b></th>
+                        <th><b>Title</b></th>
+                        <th><b>Author</b></th>
+                        <th><b>Categories</b></th>
+                        <th><b>Stock</b></th>
+                        <th><b>Price</b></th>
+                        <th><b>Action</b></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($books as $book)
+                        <tr>
+                            <td>
+                                @if ($book->cover)
+                                    <img src="{{ asset('storage/' . $book->cover) }}" width="96px">
+                                @endif
+                            </td>
+                            <td>
+                                {{ $book->title }}
+                            </td>
+                            <td>{{ $book->author }}</td>
+                            <td>
+                                <ul class="pl-3">
+                                    @foreach ($book->categories as $category)
+                                        <li>{{ $category->name }}</li>
+                                    @endforeach
+                                </ul>
+                            </td>
+                            <td>{{ $book->stock }}</td>
+                            <td>{{ $book->price }}</td>
+                            <td>
+                                <form method="POST" class="d-inline" action="{{ route('books.restore', [$book->id]) }}">
+
+                                    @csrf
+
+                                    <input type="submit" value="Restore" class="btn btn-success">
+                                </form>
+
+
+                                <form method="POST" class="d-inline"
+                                    onsubmit="return confirm('Delete this book permanently?')"
+                                    action="{{ route('books.delete-permanent', [$book->id]) }}">
+                                    @csrf
+
+                                    <input type="hidden" name="_method" value="DELETE">
+
+                                    <input type="submit" value="Delete" class="btn btn-danger btn-sm">
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colspan="10">
+                            {{ $books->appends(Request::all())->links() }}
+                        </td>
+                    </tr>
+                </tfoot>
+            </table>
+        </div>
+    </div>
+@endsection
