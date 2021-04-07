ab246414da2b129e2b3bae179364869f8b337aed
mudherdomili <mudher.d@gmail.com>
2021-04-08 01:12:17 +0800

    Penambahan menu sidebar Manage Books

--- resources/views/layouts/global.blade.php
+++ resources/views/layouts/global.blade.php
@@ -86,10 +86,10 @@
                     </li>
                     <li>
                         <a href="{{ route('categories.index') }}">
-                        <span class="oi oi-tag"></span>Manage Categories
+                            <span class="oi oi-tag"></span>Manage Categories
                         </a>
                     </li>
-
+                    <li><a href="{{ route('books.index') }}"><span class="oi oi-book"></span>Manage Books</a></li>
                     <div class="d-block d-md-none">
                         <div class="dropdown-divider"></div>
                         <li><a href="#">Profile</a></li>
